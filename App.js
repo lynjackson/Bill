9f598eab1caade0739db924c7c094f2030f200fa
bklyn <lynjacksonoconnor@gmail.com>
2020-02-24 09:49:50 -0500

    about to branch

--- App.js
+++ App.js
@@ -1,18 +1,3 @@
-// Thanks for checking out Bill! Bill is an application that takes you through the dining experience using your mobile phone. The app allows you and your friends to send orders directly to a restaurant's kitchen when dining in. When your party is finished dining, you can then pay through your phone using our convenient payment options. Enjoy!
-
-// Instructions:
-
-// 1 - Select the 'iOS' option at the top of the panel to the right.
-
-// 2 - Select 'Tap to play'.
-
-// 3 - Enter your name and hit 'SIGN IN'. Don't worry about a password.
-
-// 4 - Please accept my apologies if there is a queue. There are currently very few options for sharing development mode React Native projects, short of publishing to the app store.
-
-// Thanks. -Lyn
-
-
 
 import React from 'react';
 import { DrawerNavigator, StackNavigator, DrawerItems, SafeAreaView } from 'react-navigation'
